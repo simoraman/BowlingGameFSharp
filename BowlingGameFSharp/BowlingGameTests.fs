8490e9a1d5c9a7618cc7edafb7896959fc3e87ca
Simo Raman <simoraman@gmail.com>
2013-01-30 15:42:44 +0200

    removed game-class

--- BowlingGameFSharp/BowlingGameTests.fs
+++ BowlingGameFSharp/BowlingGameTests.fs
@@ -9,34 +9,29 @@ let throwmany(amount, pins) = [ for n in 1..amount -> pins ]
 type BowlingGameTests()=   
     [<Test>]
     member x.``Bowling guttergame``() =
-        let game=new Game()
         let throws = throwmany(20, 0)
-        game.score throws |> should equal 0
-
+        score throws |> should equal 0
+        
     [<Test>]
     member x.``Throw all ones``() =
-        let game=new Game()
         let throws = throwmany(20, 1) 
-        game.score throws |> should equal 20
+        score throws |> should equal 20
 
     [<Test>]
     member x.``Throw one spare``() =
-        let game=new Game()
         let sparethrows = [5; 5; 3]
         let manythrows=throwmany (17, 0)
         let throws = sparethrows @ manythrows
-        game.score throws |> should equal 16
+        score throws |> should equal 16
 
     [<Test>]
     member x.``Throw one strike``() =
-        let game=new Game()
         let throws = [10; 3; 4] @ throwmany (16, 0)
-        game.score throws |> should equal 24
+        score throws |> should equal 24
 
     [<Test>]
     member x.``Perfect game``() =
-        let game=new Game()
         let throws = throwmany (12, 10)
-        game.score throws |> should equal 300
+        score throws |> should equal 300
 
   
--- BowlingGameFSharp/Game.fs
+++ BowlingGameFSharp/Game.fs
@@ -1,19 +1,17 @@
 ﻿module BowlingGameFSharp
         
-let calculate_strike_score (throws: int list) = 
+let private calculate_strike_score (throws: int list) = 
     if throws.Length>3 then 10+throws.Item(1)+throws.Item(2)
     else 10
 
-let rec score_throws (throws: int list) (score:int) =
+let rec private score_throws (throws: int list) (score:int) =
     match throws with
     | [] -> score
     | throws when throws.Head = 10 -> score_throws throws.Tail score+calculate_strike_score(throws) 
     | throws when (throws.Head+throws.Tail.Head = 10) -> score_throws throws.Tail.Tail (score+10+throws.Item(2))
     | _ -> score_throws throws.Tail.Tail score+throws.Head+throws.Tail.Head
-        
-type Game =
-    new()={}
-    member x.score throws = score_throws throws 0
+
+let score throws = score_throws throws 0
         
 
         
