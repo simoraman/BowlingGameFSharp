891abccd47351ac5121c005b0d3235b5a13dbbc5
Simo Raman <simoraman@gmail.com>
2013-01-31 12:45:31 +0200

    added spare calculation to own function for clarity.

--- BowlingGameFSharp/Game.fs
+++ BowlingGameFSharp/Game.fs
@@ -4,14 +4,16 @@ let private calculate_strike_score (throws: int list) =
     if throws.Length>3 then 10+throws.Item(1)+throws.Item(2)
     else 10
 
+let private calculate_spare_score (throws:int list) = 10+throws.Item(2)
+
 let rec private score_throws (throws: int list) (score:int) =
     match throws with
     | [] -> score
-    | throws when throws.Head = 10 -> score_throws throws.Tail score+calculate_strike_score(throws) 
-    | throws when (throws.Head+throws.Tail.Head = 10) -> score_throws throws.Tail.Tail (score+10+throws.Item(2))
-    | _ -> score_throws throws.Tail.Tail score+throws.Head+throws.Tail.Head
+    | throws when throws.Head = 10 
+        -> score_throws throws.Tail score + calculate_strike_score(throws) 
+    | throws when (throws.Head + throws.Tail.Head = 10) 
+        -> score_throws throws.Tail.Tail score + calculate_spare_score(throws)
+    | _ -> score_throws throws.Tail.Tail score + throws.Head+throws.Item(1)
 
 let score throws = score_throws throws 0
-        
-
         
